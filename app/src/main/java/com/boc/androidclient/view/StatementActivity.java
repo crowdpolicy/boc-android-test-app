4407cfe1bee10d951fb3d107fa493bfc152752e5
Christos Zacharopoulos <chris@crowdpolicy.com>
2019-11-20 17:18:36 +0200

    BAFS-64

--- app/src/main/java/com/boc/androidclient/view/StatementActivity.java
+++ app/src/main/java/com/boc/androidclient/view/StatementActivity.java
@@ -9,6 +9,13 @@ import android.widget.TextView;
 
 import com.boc.androidclient.R;
 import com.boc.androidclient.utils.Utilities;
+import com.boc.client.model.Transaction;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -20,7 +27,14 @@ public class StatementActivity extends AppCompatActivity {
     private final String LOGTAG = this.getClass().getName();
     private Utilities utils = new Utilities();
 
-    private TextView statement;
+    private TextView transId;
+    private TextView transDcind;
+    private TextView transAmount;
+    private TextView transCurrency;
+    private TextView transDesc;
+    private TextView transPostingdate;
+    private TextView transValuedate;
+    private TextView transType;
 
 
     @Override
@@ -29,9 +43,34 @@ public class StatementActivity extends AppCompatActivity {
         setContentView(R.layout.activity_statement);
 
         String statementAsString = getIntent().getStringExtra("transactionArray");
-        statement = findViewById(R.id.statement_list);
+        Type collectionType = new TypeToken<List<Transaction>>(){}.getType();
+        List<Transaction> list = new Gson()
+                .fromJson( statementAsString , collectionType);
 
-        statement.setText(statementAsString);
+        //TODO recyclerview, only displays the first transaction
+        transId = findViewById(R.id.trans_id);
+        transId.setText("TransactionID: "+ list.get(0).getId());
+
+        transDcind = findViewById(R.id.trans_dcind);
+        transDcind.setText("DcInd: "+ list.get(0).getDcInd());
+
+        transAmount = findViewById(R.id.trans_amount);
+        transAmount.setText("Amount: "+ list.get(0).getTransactionAmount().getAmount().toString());
+
+        transCurrency = findViewById(R.id.trans_currency);
+        transCurrency.setText("Currency: "+list.get(0).getTransactionAmount().getCurrency());
+
+        transDesc = findViewById(R.id.trans_description);
+        transDesc.setText("Description: "+list.get(0).getDescription());
+
+        transPostingdate = findViewById(R.id.trans_postingdate);
+        transPostingdate.setText("PostingDate: "+list.get(0).getPostingDate());
+
+        transValuedate = findViewById(R.id.trans_valuedate);
+        transValuedate.setText("ValueDate: "+list.get(0).getValueDate());
+
+        transType = findViewById(R.id.trans_type);
+        transType.setText("Type: "+list.get(0).getTransactionType());
 
     }
 
